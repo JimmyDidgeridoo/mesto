c87583cfe8bea6aef83250ae6284578dab78322d
JimmyDidgeridoo <buksir44@gmail.com>
2021-07-15 16:20:39 +0300

    add show/close popup js

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,15 @@
-let editButton = document.querySelector(".profile__edit-button");
+let editButton = document.querySelector('.profile__edit-button');
+let showPopup = document.querySelector('.popup');
+
+editButton.addEventListener('click', function() {
+    showPopup.classList.add('popup__opened');
+});
+
+let closeButton = document.querySelector('.popup__close-button')
+let closePopup = document.querySelector('.popup');
+
+closeButton.addEventListener('click', function() {
+    closePopup.classList.remove('popup__opened');
+});
+
+
